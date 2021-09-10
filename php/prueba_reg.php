f19375e7bb89ac4ba4e4aec1edef00f2384b2053
Naifox12 <90470479+Naifox12@users.noreply.github.com>
2021-09-10 11:49:43 -0500

    Add files via upload

---
+++ php/prueba_reg.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <link rel="stylesheet" href="estilis_login.css">
+        <meta charset="utf-8">
+        <title>Bienvenido</title>
+    </head>
+    <body>
+        <section class="form_login">
+            <form method="post">
+            <h5>Bienvenido</h5>
+            <input class="controls" type="text" name="correo" value="" placeholder="Correo Electrónico">
+            <input class="controls" type="password" name="contraseña" value="" placeholder= "Contraseña">
+            <p><a href="#">¿Olvidaste la Contraseña?</a></p>
+            <p class="c"><a href="#">Crear una cuenta</a></p>
+            <input class="buttons" type="submit" name="register" value="INGRESAR">
+            </form>
+            <?php
+            ?>
+        </section>
+
+    </body>
+</html>
